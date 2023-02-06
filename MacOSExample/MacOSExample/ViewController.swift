6244d1b5a064038d855a2c3e1703af660800a71d
wnh <wnh@meitu.com>
2023-02-06 10:21:43 +0800

    [Optimization] slider draw

--- MacOSExample/MacOSExample/ViewController.swift
+++ MacOSExample/MacOSExample/ViewController.swift
@@ -13,12 +13,24 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var window: Window?
-        window?.title = ""
-
-
+        let slider = Slider(frame: NSRect(x: 20, y: 20, width: 300, height: 30))
+        slider.target = self
+        slider.action = #selector(didChangeValue(slider:))
+        slider.minValue = 0
+        slider.maxValue = 10
+        slider.intValue = 2
+        slider.barColor = .separatorColor
+        slider.progressColor = .controlAccentColor
+        slider.knobColor = .orange
+        slider.knobSize = CGSize(width: 20, height: 20)
+        slider.barHeight = 18
+        slider.barRadius = .normal
+        view.addSubview(slider)
+    }
+    
+    @objc func didChangeValue(slider: Slider) {
+//        print(slider.floatValue)
     }
 
 
 }
-
