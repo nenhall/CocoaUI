5ec3b505e15f87184b67a6a9b33c8e87904f913a
wnh <wnh@meitu.com>
2023-03-03 17:34:24 +0800

    add NSPanel

---
+++ Sources/macOS/AppKit/Panel.swift
@@ -0,0 +1,61 @@
+//
+//  Panel.swift
+//  DeveloperBox
+//
+//  Created by meitu@nenhall on 2022/9/30.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+public struct Panel {
+    
+    @available(macOS 11.0, *)
+    public static func showOpen(fileTypes: [UTType]? = nil,
+                         title: String? = nil,
+                         message: String? = nil,
+                         multipleSelection: Bool = false) -> [URL]? {
+        return Self.showOpen(fileTypes: fileTypes?.map({ $0.identifier }), title: title, message: message, multipleSelection: multipleSelection)
+    }
+    
+    public static func showOpen(fileTypes: [String]? = nil,
+                         title: String? = nil,
+                         message: String? = nil,
+                         multipleSelection: Bool = false) -> [URL]? {
+        let panel = NSOpenPanel()
+        panel.allowedFileTypes = fileTypes
+        panel.canCreateDirectories = false
+        panel.isExtensionHidden = false
+        panel.allowsOtherFileTypes = false
+        panel.canChooseDirectories = true
+        panel.allowsMultipleSelection = multipleSelection
+        if let title = title {
+            panel.title = title
+        }
+        if let message = message {
+            panel.message = message
+        }
+        panel.center()
+        let response = panel.runModal()
+        return response == .OK ? panel.urls : nil
+    }
+    
+    public static func showSave(directoryPath: String, title: String? = nil, message: String? = nil, prompt: String? = nil) -> URL? {
+        let panel = NSSavePanel()
+        panel.canCreateDirectories = true
+        panel.isExtensionHidden = false
+        panel.directoryURL = URL(fileURLWithPath: directoryPath)
+        if let title = title {
+            panel.title = title
+        }
+        if let message = message {
+            panel.message = message
+        }
+        if let prompt = prompt {
+            panel.prompt = prompt
+        }
+        panel.center()
+        let response = panel.runModal()
+        return response == .OK ? panel.directoryURL : nil
+    }
+}
