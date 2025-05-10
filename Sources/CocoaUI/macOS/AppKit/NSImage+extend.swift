964bd46e12f1eeb3de66f8fcb6f80744306a455d
nenhall <nenhall@126.com>
2025-05-10 11:02:02 +0800

    添加给系统图片上色方法

--- Sources/CocoaUI/macOS/AppKit/NSImage+extend.swift
+++ Sources/CocoaUI/macOS/AppKit/NSImage+extend.swift
@@ -41,4 +41,16 @@ public extension NSImage {
     }
     
 }
+
+extension NSImage {
+   public func tinted(with color: NSColor) -> NSImage {
+        let image = self.copy() as! NSImage
+        image.lockFocus()
+        color.set()
+        let imageRect = NSRect(origin: .zero, size: image.size)
+        imageRect.fill(using: .sourceAtop)
+        image.unlockFocus()
+        return image
+    }
+}
 #endif
