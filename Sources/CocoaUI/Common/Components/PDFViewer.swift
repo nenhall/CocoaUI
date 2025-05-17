5774510f2d8adfeae2e5800bac2e62e4f3c99ea5
nenhall <nenhall@126.com>
2025-05-17 15:05:10 +0800

    add PDFView option hidden

--- Sources/CocoaUI/Common/Components/PDFViewer.swift
+++ Sources/CocoaUI/Common/Components/PDFViewer.swift
@@ -14,10 +14,12 @@ public struct PDFViewer: ViewRepresentable {
     
     let url: URL?
     let data: Data?
+    let hideScrollBar: Bool
     
-    public init(url: URL? = nil, data: Data? = nil) {
+    public init(url: URL? = nil, data: Data? = nil, hideScrollBar: Bool = false) {
         self.url = url
         self.data = data
+        self.hideScrollBar = hideScrollBar
     }
     
     public func makeView(context: Context) -> PDFView {
@@ -29,8 +31,38 @@ public struct PDFViewer: ViewRepresentable {
 #endif
         pdfView.autoresizingMask = autoresizingMask
         pdfView.translatesAutoresizingMaskIntoConstraints = true
+        pdfView.enclosingScrollView?.autohidesScrollers = true
+        pdfView.enclosingScrollView?.hasVerticalScroller = hideScrollBar
+        if hideScrollBar {
+            hideScrollBars(in: pdfView)
+        }
         pdfView.autoScales = true // 自动缩放
         return pdfView
+    }
+    
+    func hideScrollBars(in pdfView: PDFView) {
+        #if os(iOS)
+        for subview in pdfView.subviews {
+            if let scrollView = subview as? UIScrollView {
+                scrollView.showsHorizontalScrollIndicator = false
+                scrollView.showsVerticalScrollIndicator = false
+            }
+            for subSubview in subview.subviews {
+                if let scrollView = subSubview as? UIScrollView {
+                    scrollView.showsHorizontalScrollIndicator = false
+                    scrollView.showsVerticalScrollIndicator = false
+                }
+            }
+        }
+        #else
+        // 隐藏滚动条
+        if let scrollView = pdfView.enclosingScrollView {
+            scrollView.hasHorizontalScroller = false
+            scrollView.hasVerticalScroller = false
+        }
+        pdfView.displaysPageBreaks = false  // 隐藏分页符
+        pdfView.displayMode = .singlePage   // 单页模式（禁止滚动）
+        #endif
     }
     
     public func updateView(_ uiView: PDFView, context: Context) {
